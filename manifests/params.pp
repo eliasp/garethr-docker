bb592d0c10dd76d9359fa68733ab2ac0008b24d1
Elias Probst <mail@eliasprobst.eu>
2013-08-27 22:12:55 +0200

    Started adding class documentation for the params manifest.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,13 @@
+# == Class: docker::params
+#
+# Initializes and provides the parameters for the docker module
+#
+# === Parameters
+#
+# [*version*]
+#   Which version of the lxc-docker should be installed? Defaults to the
+#   'latest' available version.
+#
 class docker::params {
   $version = present
 }
